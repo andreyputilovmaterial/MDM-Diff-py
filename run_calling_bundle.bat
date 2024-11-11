99142f0561684b98eea3c783f99175e402aa10f8
Andrey Putilov <Andrey.Putilov@materialplus.io>
2024-11-10 20:03:38 -0800

    BAT files: right file name for diff json (using pyhton -c)

--- run_calling_bundle.bat
+++ run_calling_bundle.bat
@@ -8,7 +8,7 @@ SET "MDD_B=examples\p221366_v77.mdd"
 set "MDD_A_JSON=%MDD_A%.json"
 set "MDD_B_JSON=%MDD_B%.json"
 
-set "MDD_FINAL_DIFF_JSON=examples\report.diff.p221366_wave19.mdd-p221366_v77.mdd.json"
+FOR /F "delims=" %%i IN ('python -c "import sys;from pathlib import Path;import re;inp_mdd_l = sys.argv[1];inp_mdd_r = sys.argv[2];report_part_mdd_left_filename = re.sub( r'\.mdd\.json', '.mdd', Path(inp_mdd_l).name );report_part_mdd_right_filename = re.sub( r'\.mdd\.json', '.mdd', Path(inp_mdd_r).name );report_filename = 'report.diff.{mdd_l}-{mdd_r}.json'.format(mdd_l=report_part_mdd_left_filename,mdd_r=report_part_mdd_right_filename);result_json_fname = ( Path(inp_mdd_l).parents[0] / report_filename );print(result_json_fname)" "%MDD_A%" "%MDD_B%"') DO SET "MDD_FINAL_DIFF_JSON=%%i"
 
 
 ECHO -
