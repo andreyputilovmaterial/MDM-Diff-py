fdf938d5efb4b3b2cec4776f3577c7846ff48874
Andrey Putilov <Andrey.Putilov@materialplus.io>
2024-11-10 20:33:47 -0800

    better naming of distributable files

--- make.bat
+++ make.bat
@@ -10,12 +10,25 @@ if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && exit /b %errorlevel% )
 ECHO Done
 
 ECHO Pathcing bundle.py...
+ECHO # ... >> dist/bundle.py
 ECHO # print('within bundle') >> dist/bundle.py
 REM REM :: no need for this, the root package is loaded automatically
 @REM ECHO # import bundle >> dist/bundle.py
 ECHO from src import run_universal >> dist/bundle.py
 ECHO run_universal.main() >> dist/bundle.py
 ECHO # print('out of bundle') >> dist/bundle.py
+
+PUSHD dist
+COPY ..\run_calling_bundle.bat .\run_mdd_diff.bat
+COPY ..\run_calling_bundle_with_routing.bat .\run_mdd_diff_with_routing.bat
+COPY ..\run_calling_bundle_with_translations.bat .\run_mdd_diff_with_translations.bat
+COPY ..\run_calling_bundle_mdd_report.bat .\run_mdd_report.bat
+REN bundle.py mdmtoolsap.py
+powershell -Command "(gc 'run_mdd_diff.bat' -encoding 'Default') -replace '(dist[/\\])?bundle.py', 'mdmtoolsap.py' | Out-File -encoding 'Default' 'run_mdd_diff.bat'"
+powershell -Command "(gc 'run_mdd_diff_with_routing.bat' -encoding 'Default') -replace '(dist[/\\])?bundle.py', 'mdmtoolsap.py' | Out-File -encoding 'Default' 'run_mdd_diff_with_routing.bat'"
+powershell -Command "(gc 'run_mdd_diff_with_translations.bat' -encoding 'Default') -replace '(dist[/\\])?bundle.py', 'mdmtoolsap.py' | Out-File -encoding 'Default' 'run_mdd_diff_with_translations.bat'"
+powershell -Command "(gc 'run_mdd_report.bat' -encoding 'Default') -replace '(dist[/\\])?bundle.py', 'mdmtoolsap.py' | Out-File -encoding 'Default' 'run_mdd_report.bat'"
+POPD
 
 @REM ECHO Clear up ..\test_pinliner_results\...
 @REM PUSHD ..\test_pinliner_results
