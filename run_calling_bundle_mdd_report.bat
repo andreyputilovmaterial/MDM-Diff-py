b13dda06402bf01f4e79507f8206d20ca4f4f56c
The-city-not-present <anputilov@mail.ru>
2024-11-12 09:13:04 +0300

    making compatible with python 3.12, updated pinliner is now part of the distribution

--- run_calling_bundle_mdd_report.bat
+++ run_calling_bundle_mdd_report.bat
@@ -11,18 +11,19 @@ ECHO -
 ECHO 1. read MDD A
 ECHO read from: %MDD_A%
 ECHO write to: .json
-python dist/bundle.py --program read --mdd "%MDD_A%" --config-features label,properties --config-section mdmproperties,languages,shared_lists,fields
-if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && exit /b %errorlevel% )
+python dist/mdmtoolsap_bundle.py --program read --mdd "%MDD_A%" --config-features label,properties --config-section mdmproperties,languages,shared_lists,fields
+if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && goto CLEANUP && exit /b %errorlevel% )
 
 ECHO -
 ECHO 2. generate html
-python dist/bundle.py --program report --inpfile "%MDD_A_JSON%"
-if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && exit /b %errorlevel% )
+python dist/mdmtoolsap_bundle.py --program report --inpfile "%MDD_A_JSON%"
+if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && goto CLEANUP && exit /b %errorlevel% )
 
 ECHO -
+:CLEANUP
 ECHO 999. Clean up
 REM REM :: comment: just deleting trach .pyc files after the execution - they are saved when modules are loaded from within bndle file created with pinliner
-REM REM :: however, it is necessary to delete these .pyc files before every call of the bundle
+REM REM :: however, it is necessary to delete these .pyc files before every call of the mdmtoolsap_bundle
 REM REM :: it means, 6 more times here, in this script; but I don't do it cause I have this added to the linliner code - see my pinliner fork
 DEL *.pyc
 IF EXIST __pycache__ (
@@ -33,4 +34,5 @@ RMDIR /Q /S __pycache__
 )
 
 ECHO done!
+exit /b %errorlevel%
 
