b645dc20c1d7dec3e5b12bd1ede709cd81de5c3c
Andrey Putilov <Andrey.Putilov@materialplus.io>
2025-01-22 18:55:47 -0800

    now can read HelperFields; sync submodule version

--- src/helper_utility_wrappers.py
+++ src/helper_utility_wrappers.py
@@ -14,6 +14,7 @@ class PerformanceMonitor:
         self.userprovideddata_totalrecords = config['total_records'] if 'total_records' in config else None
         self.config_frequency_records = config['report_frequency_records_count'] if 'report_frequency_records_count' in config else None
         self.config_frequency_timeinterval = config['report_frequency_timeinterval'] if 'report_frequency_timeinterval' in config else None
+        self.config_text_pipein = config['report_text_pipein'] if 'report_text_pipein' in config and config['report_text_pipein'] else 'progress'
     
     def __iter__(self):
         self.progress = 0
@@ -27,10 +28,11 @@ class PerformanceMonitor:
         if (self.config_frequency_records is None) or (self.progress - self.progress_last_reported > self.config_frequency_records):
             time_now = time.time()
             if (self.config_frequency_timeinterval is None) or ((time_now - self.time_last_reported)>self.config_frequency_timeinterval):
-                print( 'progress: processing {nline}{display_out_total}{display_percents}'.format(
+                print( '{text_pipe}: processing {nline}{display_out_total}{display_percents}'.format(
                     nline = self.progress,
                     display_out_total = ' / {nlinetotal}'.format(nlinetotal=self.userprovideddata_totalrecords) if (self.userprovideddata_totalrecords is not None) else '',
-                    display_percents=' ({per}%)'.format(per=round(self.progress*100/self.userprovideddata_totalrecords,1)) if (self.userprovideddata_totalrecords is not None) else ''
+                    display_percents=' ({per}%)'.format(per=round(self.progress*100/self.userprovideddata_totalrecords,1)) if (self.userprovideddata_totalrecords is not None) else '',
+                    text_pipe = self.config_text_pipein,
                 ))
                 self.progress_last_reported = self.progress
                 self.time_last_reported = time_now
