a9ce499d113f88d8e1da12160a5802fedb29bcfb
Andrey Putilov <Andrey.Putilov@materialplus.io>
2025-01-25 16:12:59 -0800

    read_spss: configurable if we should inlcude data or just read metadata only; performance metrics

---
+++ src/lib/otherformatsreadpy_spss/helper_utility_performancemonitor.py
@@ -0,0 +1,40 @@
+
+
+import time
+
+
+
+class PerformanceMonitor:
+    def __init__(self,config={}):
+        self.__config = config
+        self.progress = None
+        self.time_started = None
+        self.time_last_reported = None
+        self.progress_last_reported = None
+        self.userprovideddata_totalrecords = config['total_records'] if 'total_records' in config else None
+        self.config_frequency_records = config['report_frequency_records_count'] if 'report_frequency_records_count' in config else None
+        self.config_frequency_timeinterval = config['report_frequency_timeinterval'] if 'report_frequency_timeinterval' in config else None
+        self.config_text_pipein = config['report_text_pipein'] if 'report_text_pipein' in config and config['report_text_pipein'] else 'progress'
+    
+    def __iter__(self):
+        self.progress = 0
+        self.time_started = time.time()
+        self.time_last_reported = self.time_started
+        self.progress_last_reported = -1
+        return self
+    
+    def __next__(self):
+        self.progress = self.progress + 1
+        if (self.config_frequency_records is None) or (self.progress - self.progress_last_reported > self.config_frequency_records):
+            time_now = time.time()
+            if (self.config_frequency_timeinterval is None) or ((time_now - self.time_last_reported)>self.config_frequency_timeinterval):
+                print( '{text_pipe}: processing {nline}{display_out_total}{display_percents}'.format(
+                    nline = self.progress,
+                    display_out_total = ' / {nlinetotal}'.format(nlinetotal=self.userprovideddata_totalrecords) if (self.userprovideddata_totalrecords is not None) else '',
+                    display_percents=' ({per}%)'.format(per=round(self.progress*100/self.userprovideddata_totalrecords,1)) if (self.userprovideddata_totalrecords is not None) else '',
+                    text_pipe = self.config_text_pipein,
+                ))
+                self.progress_last_reported = self.progress
+                self.time_last_reported = time_now
+        return None
+
--- src/lib/otherformatsreadpy_spss/read.py
+++ src/lib/otherformatsreadpy_spss/read.py
@@ -8,15 +8,15 @@ import pandas as pd
 
 
 
-    # if __name__ == '__main__':
-    #     # run as a program
-    #     from lib.markitdown.src.markitdown._markitdown import MarkItDown
-    # elif '.' in __name__:
-    #     # package
-    #     from .lib.markitdown.src.markitdown._markitdown import MarkItDown
-    # else:
-    #     # included with no parent package
-    #     from lib.markitdown.src.markitdown._markitdown import MarkItDown
+if __name__ == '__main__':
+    # run as a program
+    import helper_utility_performancemonitor
+elif '.' in __name__:
+    # package
+    from . import helper_utility_performancemonitor
+else:
+    # included with no parent package
+    import helper_utility_performancemonitor
 
 
 
@@ -37,22 +37,11 @@ JSON_TEMPLATE = r'''
         },
         "flags": [ "data-type:spss" ]
     },
-    "sections": [
-        {
-            "name": "variables",
-            "columns": ["name","label"],
-            "content": []
-        },
-        {
-            "name": "data",
-            "columns": [],
-            "content": []
-        }
-    ]
+    "sections": []
 }
 '''
 
-def read(file_data):
+def read(file_data,config={}):
 
     inp_file = file_data['filename']
 
@@ -62,25 +51,61 @@ def read(file_data):
     result['report_datetime_utc']='{f}'.format(f=(datetime.now()).astimezone(tz=timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),)
     result['report_datetime_local']='{f}'.format(f=(datetime.now()).strftime('%Y-%m-%dT%H:%M:%SZ'))
 
+    print('reading spss...')
     df = pd.read_spss(inp_file)
     df_mdata = df.attrs
 
+    print('done, working on it...')
     result_report_scheme = result['report_scheme']
     result_report_properties = result['source_file_metadata']
-    result_section_variables = result['sections'][0]
-    result_section_data = result['sections'][1]
-    result_variables = result_section_variables['content']
-    result_data = result_section_data['content']
+    result_section_variables = None
+    result_section_value_labels = None
+    result_section_data = None
+    if 'read_variables' in config and config['read_variables']:
+        result['sections'].append({'name':'variables','columns':['name','label'],'content':[]})
+        result_section_variables = result['sections'][-1]
+    if 'read_value_labels' in config and config['read_value_labels']:
+        result['sections'].append({'name':'value labels','columns':['name','label'],'content':[]})
+        result_section_value_labels = result['sections'][-1]
+    if 'read_data' in config and config['read_data']:
+        result['sections'].append({'name':'data','columns':[],'content':[]})
+        result_section_data = result['sections'][-1]
+    result_variables = result_section_variables['content'] if result_section_variables else None
+    result_value_labels = result_section_value_labels['content'] if result_section_value_labels else None
+    result_data = result_section_data['content'] if result_section_data else None
 
+    print('reading metadata...')
     for attr_name, attr_value in df_mdata.items():
-        if attr_name in ['column_names''column_labels','column_names_to_label']:
+        if attr_name in [
+            'column_names',
+            'column_labels',
+            'column_names_to_labels',
+            'variable_value_labels',
+            'variable_to_label',
+            'value_labels',
+            'original_variable_types',
+            'readstat_variable_types',
+            'variable_storage_width',
+            'variable_display_width',
+            'variable_alignment',
+            'variable_measure',
+        ] or attr_name[:len('readstat_')]=='readstat_':
             # skip these
             continue
         attr_value_clean = '{v}'.format(v=attr_value)
         if isinstance(attr_value,dict) or isinstance(attr_value,list):
             # attr_value_clean = '[ ' + ', '.join([m for m in attr_value]) + ' ]'
             attr_value_clean = json.dumps(attr_value)
+            if len(attr_value_clean)>64:
+                attr_value_clean = attr_value_clean[:63]+'...'
         result_report_properties.append({'name':attr_name,'value':attr_value_clean})
+
+    if result_section_value_labels:
+        for variable_name, cat_data in df_mdata['variable_value_labels'].items():
+            for cat_value_value, cat_label in cat_data.items():
+                item_name = '{variable_name}.Values[{cat_name}]'.format(variable_name=variable_name,cat_name=cat_value_value)
+                item_label = cat_label
+                result_value_labels.append({'name':item_name,'label':item_label})
 
     for col in df.columns:
         col_txt = '{t}'.format(t=col)
@@ -88,15 +113,27 @@ def read(file_data):
         col_label = df_mdata['column_names_to_labels'][col_txt]
         result_report_scheme['columns'].append(col_name)
         result_report_scheme['column_headers'][col_name] = col_label
-        result_section_data['columns'].append(col_name)
-        result_variables.append({'name':col_name,'label':col_label})
+        if result_section_data:
+            result_section_data['columns'].append(col_name)
+        if result_section_variables:
+            result_variables.append({'name':col_name,'label':col_label})
 
-    for casenumber in range(0,len(df)):
-        row = df.iloc[casenumber,]
-        result_row = {}
-        for col in df.columns:
-            result_row['{c}'.format(c=col)] = row[col]
-        result_data.append( result_row )
+    if result_section_data:
+        print('reading data...')
+        performance_counter = iter(helper_utility_performancemonitor.PerformanceMonitor(config={
+            'total_records': len(df),
+            'report_frequency_records_count': 1,
+            'report_frequency_timeinterval': 9,
+        }))
+        for casenumber in range(0,len(df)):
+            row = df.iloc[casenumber,]
+            next(performance_counter)
+            result_row = {}
+            for col in df.columns:
+                result_row['{c}'.format(c=col)] = row[col]
+            result_data.append( result_row )
+    
+    print('done')
 
     return result
 
@@ -113,16 +150,48 @@ def entry_point(config={}):
         type=str,
         required=True
     )
+    def arg_str2bool(value):
+        if isinstance(value,bool):
+            return value
+        elif isinstance(value,int) or isinstance(value,float):
+            return not not value
+        elif re.match(r'^\s*?$',value,flags=re.I|re.DOTALL):
+            return False
+        elif re.match(r'^\s*?(?:true|1|yes)\s*?$',value,flags=re.I|re.DOTALL):
+            return True
+        elif re.match(r'^\s*?(?:false|0|no)\s*?$',value,flags=re.I|re.DOTALL):
+            return False
+        else:
+            try:
+                value = int(value)
+                return not not value
+            except:
+                pass
+            try:
+                value = float(value)
+                return not not value
+            except:
+                pass
+                return not not value
     parser.add_argument(
         '--config-read-variables',
         help='Config option: include info about the variables',
-        type=str,
+        type=arg_str2bool,
+        default=True,
+        required=False
+    )
+    parser.add_argument(
+        '--config-read-value-labels',
+        help='Config option: include info about the variables',
+        type=arg_str2bool,
+        default=True,
         required=False
     )
     parser.add_argument(
         '--config-read-data',
         help='Config option: include data (can be slow, or very slow, even on smallest projects)',
-        type=str,
+        type=arg_str2bool,
+        default=False,
         required=False
     )
     args = None
@@ -136,10 +205,16 @@ def entry_point(config={}):
     inp_file = '{inp_file}'.format(inp_file=inp_file.resolve())
     if not(Path(inp_file).is_file()):
         raise FileNotFoundError('file not found: {fname}'.format(fname=inp_file))
+    
+    config_spss_reader = {
+        'read_variables': args.config_read_variables,
+        'read_value_labels': args.config_read_value_labels,
+        'read_data': args.config_read_data,
+    }
 
     print('reading file: opening {fname}, script started at {dt}'.format(dt=time_start,fname=inp_file))
 
-    data = read({'filename':inp_file})
+    data = read({'filename':inp_file},config=config_spss_reader)
 
     result_json_fname = ( Path(inp_file).parents[0] / '{basename}{ext}'.format(basename=Path(inp_file).name,ext='.json') if Path(inp_file).is_file() else re.sub(r'^\s*?(.*?)\s*?$',lambda m: '{base}{added}'.format(base=m[1],added='.json'),'{path}'.format(path=inp_file)) )
     print('reading file: saving as "{fname}"'.format(fname=result_json_fname))
